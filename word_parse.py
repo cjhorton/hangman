809135fea9c806eed155be60473d4759f296f69e
CJ Horton <jayhorton@gmail.com>
2021-01-11 12:25:41 -0600

    Added additional regex to check for acronym and a arrange list function.

--- word_parse.py
+++ word_parse.py
@@ -7,15 +7,13 @@ def parse_words_file(words_path, words):
     try:
       with open(words_path, "r") as f:
         #we want words that are 3 or more letters long
-        pattern = re.compile(r"[a-zA-Z]{3,}$")
         parsed_line = ""
         for line in f:
           parsed_line = parse_word_line(line)
-          #print(parsed_line)
-          if word_is_valid(parsed_line, pattern):
+          if word_is_valid(parsed_line):
             #add new line char at the end since will be using writelines()
-            words.append(parsed_line + "\n")
-            #print(parsed_line)
+            words.append(process_word(parsed_line) + "\n")
+
     except IOError:
       print("Unable to locate the words file.  Please check the path and try again.")
     except EOFError:
@@ -28,11 +26,24 @@ def parse_word_line(line):
     return line
 
 
-def word_is_valid(line, pattern):
-    line = line.strip()
-    return re.match(pattern, line)
+def word_is_valid(line):
+    """Word is valid if it contains only alpha and not all capital (not an acronym)"""
+    pattern1 = re.compile(r"[a-zA-Z]{3,}$")
+    pattern2 = re.compile(r"[A-Z]{3,}$")
+    return re.match(pattern1, line) and not re.match(pattern2, line)
+
+def process_word(word):
+    """Performs processing of a valid word.  Currrently converts to lowercase."""
+    p_word = word.lower()
+    return p_word
+
+def arrange_word_list(words):
+    """Sorts the word list alphabetically and by length"""
+    words.sort()
+    words.sort(key=len)
 
 def create_word_bank_file(words, path):
+    """Created the word bank file that will be used for the game"""
     with open(path, "w") as f:
       f.writelines(words)
 
@@ -41,7 +52,7 @@ def main():
     input_file = "test_words"
     words = []
     parse_words_file(input_file, words)
-    #print(words)
+    arrange_word_list(words)
     output_file = "hangman_word_bank"
     create_word_bank_file(words, output_file)
 
