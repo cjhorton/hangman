1e6473b19410dad6bccdbcc483c19231b448bc5a
CJ Horton <jayhorton@gmail.com>
2021-01-12 11:46:32 -0600

    Added populate_difficulty_brackets

--- hangman.py
+++ hangman.py
@@ -1,16 +1,70 @@
-
+import linecache
+from os import path
 from gallows import create_gallows
+
+class WordBankError(Exception):
+    """Raised when either the word bank or word bank idx files aren't found"""
+    pass
 
 def print_round(gallows, round):
     clear_screen()
     for i in gallows[round]:
         print(i)
 
+def word_bank_files_exist(bank_path, idx_path):
+    return  path.exists(bank_path) and path.exists(idx_path)
+
+def run():
+    """Game loop"""
+    pass
+
+def populate_difficulty_brackets(idx_path):
+    brackets = {"easy": [3, -1, -1], "medium": [7, -1, -1], "hard": [13, -1, -1]} #diff:[length min, start line, end line]
+    with open(idx_path, "r") as f:
+        num = 1
+        lines = f.readlines()
+        for line in lines:
+            line = line.strip()
+            if num == 1:
+                first, last = line.split(":")
+                brackets["hard"][2] = int(last)
+            else:
+                length, start_line = line.split(",")
+                length = int(length)
+                start_line = int(start_line)
+                if length >= brackets["easy"][0] and length < brackets["medium"][0]: #easy
+                    if brackets["easy"][1] == -1: #difficulty start line hasn't been set yet
+                        brackets["easy"][1] = start_line
+                elif length >= brackets["medium"][0] and length < brackets["hard"][0]: #medium
+                    if brackets["medium"][1] == -1:
+                        brackets["medium"][1] = start_line
+                    if brackets["easy"][2] == -1: #difficulty end line hasn't been set yet
+                        brackets["easy"][2] = start_line - 1 #easy ends 1 less than first medium start
+                else:
+                    if brackets["hard"][1] == -1:
+                        brackets["hard"][1] = start_line
+                    if brackets["medium"][2] == -1:
+                        brackets["medium"][2] = start_line - 1
+            num += 1
+    print(brackets)
+
 def main():
-    gallows = create_gallows()
-    print_round(gallows, 4)
+    word_bank_path = "hangman_word_bank"
+    word_bank_idx_path = "hangman_word_bank_idx"
+    try:
+        found = word_bank_files_exist(word_bank_path, word_bank_idx_path)
+        if not found:
+            raise WordBankError
+    except WordBankError:
+        print("Could not locate either the hangman_word_bank or hangman_word_bank_idx files")
+        return
+
+    populate_difficulty_brackets(word_bank_idx_path)
+    #gallows = create_gallows()
+    #print_round(gallows, 4)
 
 def clear_screen():
     print("\n" * 50)
+
 if __name__ == "__main__":
     main()
--- word_parse.py
+++ word_parse.py
@@ -50,38 +50,47 @@ def create_word_bank_file(words, path):
         print("Unable to write to the file")
 
 
-def create_word_indices(words, indices):
+def create_word_indices(words, indices, stats):
     """Creates an indices dictionary from a words list; key = word length:value = line that length starts on"""
-    line = 1
+    line = 0
     for word in words:
+        line += 1
         s_word = word.strip()
         length = len(s_word)
         if not length in indices:
             #print(f"{s_word}:{length}:{line}")
             indices[length] = line
-        line += 1
+    stats["last_line"] = line #so we can have an upper bounds for the words
 
-def create_word_indices_file(indices, path):
+def create_word_indices_file(indices, path, stats):
     """Create the word bank indicies file that will be used to find words of a certain length"""
     try:
         with open(path, "w") as f:
+            #First line in file will contain the first and last lines containing words in the word_bank file
+            first_line = stats["first_line"]
+            last_line = stats["last_line"]
+            f.write(f"{first_line}:{last_line}" + "\n")
             for length, line in indices.items():
                 f.write(f"{length},{line}" + "\n")
     except:
-        print("Unable to write to the file")
+        print("Unable to write to the indices file")
 
 def main():
+    #Parse the words file and create a list of valid words
     input_file = "/usr/share/dict/words"
-    #input_file = "test_words"
     words = []
     parse_words_file(input_file, words)
+    #Sort the words by alpha and length
     arrange_word_list(words)
+    #Create the actual word bank file that will be used
     output_file = "hangman_word_bank"
     create_word_bank_file(words, output_file)
+    #Create an index file that will, hopefully, allow for more intelligent word lookups
+    stats = {"first_line": 1, "last_line": 0}
     indices = {}
-    create_word_indices(words, indices)
+    create_word_indices(words, indices, stats)
     indices_file = "hangman_word_bank_idx"
-    create_word_indices_file(indices, indices_file)
+    create_word_indices_file(indices, indices_file, stats)
 
 if __name__ == "__main__":
   main()
