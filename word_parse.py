a711687a5ff8bd9ea15de2fde37258bd63839281
CJ Horton <jayhorton@gmail.com>
2021-01-20 12:45:27 -0600

    Modified the word_parse.py to check words against names

--- word_parse.py
+++ word_parse.py
@@ -2,14 +2,30 @@
 
 import re
 
-def parse_words_file(words_path, words):
+def create_names(names_path, names):
+    """Creates the names set that will be checked against before asdding a word to the word bank """
+    try:
+      with open(names_path, "r") as f:
+        #we want names that are 3 or more letters long
+        for line in f:
+          parsed_line = parse_word_line(line)
+          if word_is_valid(parsed_line):
+            names.add(process_word(parsed_line)) #names is a set
+    except IOError:
+      print("Unable to locate the names file.  Please check the path and try again.")
+    except EOFError:
+      print("This file appears to be empty.  Please check the file contents.")
+    print("All done parsing the names file.")
+
+
+def parse_words_file(words_path, words, names):
     """Parses the /usr/share/dict/words file into words usable by the hangman program """
     try:
       with open(words_path, "r") as f:
         #we want words that are 3 or more letters long
         for line in f:
           parsed_line = parse_word_line(line)
-          if word_is_valid(parsed_line):
+          if word_is_valid(parsed_line) and not word_is_name(parsed_line, names):
             #add new line char at the end since will be using writelines()
             words.append(process_word(parsed_line) + "\n")
 
@@ -24,12 +40,14 @@ def parse_word_line(line):
     line = line.strip()
     return line
 
-
 def word_is_valid(line):
     """Word is valid if it contains only alpha and not all capital (not an acronym)"""
     pattern1 = re.compile(r"[a-zA-Z]{3,}$")
     pattern2 = re.compile(r"[A-Z]{3,}$")
     return re.match(pattern1, line) and not re.match(pattern2, line)
+
+def word_is_name(word, names):
+   return word in names
 
 def process_word(word):
     """Performs processing of a valid word.  Currrently converts to lowercase."""
@@ -76,15 +94,23 @@ def create_word_indices_file(indices, path, stats):
         print("Unable to write to the indices file")
 
 def main():
+    #Create the names set
+    input_file = "names/names_sorted_unique.txt"
+    names = set()
+    create_names(input_file, names)
+
     #Parse the words file and create a list of valid words
     input_file = "/usr/share/dict/words"
     words = []
-    parse_words_file(input_file, words)
+    parse_words_file(input_file, words, names)
+
     #Sort the words by alpha and length
     arrange_word_list(words)
+
     #Create the actual word bank file that will be used
     output_file = "hangman_word_bank"
     create_word_bank_file(words, output_file)
+
     #Create an index file that will, hopefully, allow for more intelligent word lookups
     stats = {"first_line": 1, "last_line": 0}
     indices = {}
